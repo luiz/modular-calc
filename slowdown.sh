8b6aa979a83e8280d63fd9f6a55fb816ed2e8834
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2014-11-14 19:42:27 -0200

    slowing down to EDGE w/ slowdown.sh

--- slowdown.sh
+++ slowdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo tc qdisc add dev lo root handle 1: htb default 12
-sudo tc class add dev lo parent 1:1 classid 1:12 htb rate 500kbps
+sudo tc class add dev lo parent 1:1 classid 1:12 htb rate 250kbps
 sudo tc qdisc add dev lo parent 1:12 netem delay 300ms
