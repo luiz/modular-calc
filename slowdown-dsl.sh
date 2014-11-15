2a28be13d625fc60360f7a5ec9a0c145d02f2b6d
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2014-11-15 12:42:33 -0200

    updating timings w/ AMD based on curl and DSL speeds

---
+++ slowdown-dsl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo tc qdisc add dev lo root handle 1: htb default 12
+sudo tc class add dev lo parent 1:1 classid 1:12 htb rate 2000kbps
+sudo tc qdisc add dev lo parent 1:12 netem delay 5ms
